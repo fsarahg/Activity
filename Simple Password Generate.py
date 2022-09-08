c776e5afeebe18b78f547621c014abb7d5a07511
fsarahg <112925974+fsarahg@users.noreply.github.com>
2022-09-08 15:35:19 +0530

    Delete Simple Password Generate.py

--- Simple Password Generate.py
+++
@@ -1,5 +0,0 @@
-import random
-p = int(input("Enter length of password :"))
-x="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-y =  "".join(random.sample(x,p))
-print (y)
